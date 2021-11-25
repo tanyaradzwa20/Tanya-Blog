119fab17b3807fa95c7c0763c95a6076c197b7aa
Tanya <tanya@uncommon.org>
2021-11-25 15:40:21 +0200

    new

--- app.js
+++ app.js
@@ -3,11 +3,21 @@ const mongoose = require("mongoose");
 const app = express()
 
 
+// connecting to mongo database  
+mongoose.connect("mongdb://localhost/tanya",{
+    userNewUrlParser: true,
+    useUnifiedTopology: true},
+    () => {console.log("connected data database was successful")}
+);
+
 //  middleware
 app.use(express.urlencoded({extended: true}) )
 app.use(express.static("public"))
 app.set("view engine", "ejs")
 
+//  routes
+app.use(require("./routes/index"))
+app.use(require("./routes/compose"))
 
 
 app.listen(2000, () => console.log("server listening")) 
